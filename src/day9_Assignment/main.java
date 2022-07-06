95c456d76b1c6d30094d40fe0b5e0d393a04be8d
Dhananjay301996 <gadhavedhananjay123@gmail.com>
2022-07-06 18:39:23 +0530

    Added UC2 Program

--- src/day9_Assignment/main.java
+++ src/day9_Assignment/main.java
@@ -4,11 +4,13 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to the Address Book Main Program");
+		  System.out.println("Welcome to the Address Book Main Program");
 
-        addressbook a1 = new addressbook();
-        a1.contactList();
-        System.out.println(a1.listData);
+	        addressbook a1 = new addressbook();
+	        a1.contactList();
+	        a1.contactList();
+	        a1.contactList();
+	        System.out.println(a1.listData);
 
 	}
 
